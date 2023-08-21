6283de216f3036e82447cf963086e0740fb1b367
Klavs Tranborg <klavs@tranborg.eu>
2023-08-21 21:48:01 +0200

    Add project files.

---
+++ Week35/Class.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using Npgsql;
+namespace Week35
+{
+    public class DatabaseConnector
+    {
+        private static DatabaseConnector _instance;
+        private static string _connectionString;
+
+        private DatabaseConnector(string connectionString) {
+            _connectionString = connectionString;
+        }
+
+        public static DatabaseConnector getInstance()
+        {
+            if (_instance == null) { throw new Exception("You must Init"); }
+            return _instance;
+        }
+        
+        public static void init(string connectionString)
+        {
+            _instance = new DatabaseConnector(connectionString);
+        }
+    }
+}
