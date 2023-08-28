54c9c09457cac6770ebc45c2e6d491cd7e5dd74e
JSM80 <mail@moellers-hule.dk>
2023-08-28 12:56:41 +0200

    localhost

--- Week35/Program.cs
+++ Week35/Program.cs
@@ -6,7 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton();
+//builder.Services.AddSingleton();
 
 var app = builder.Build();
 
