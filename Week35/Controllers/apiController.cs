ee695abce47a19aec58a8f9bad6d2d0e46e7472b
Shadpre <klavs@tranborg.eu>
2023-08-29 12:12:42 +0200

    delele

---
+++ Week35/Controllers/apiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Week35.Controllers
+{
+    public class apiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
