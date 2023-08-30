31e510efff91ae52f2ef34454e7db2a7ee6622be
JSM80 <mail@moellers-hule.dk>
2023-08-30 12:09:20 +0200

    Database Jesper

--- Week35/DatabaseConnector.cs
+++ Week35/DatabaseConnector.cs
@@ -1,20 +1,28 @@
 ﻿using Dapper;
 using Npgsql;
+
 namespace Week35
 {
     public class DatabaseConnector
     {
-        private static string _connectionString;
+        private static string _connectionString = @"
+        User ID=xsxmxiiw;
+        Password=2TYFrLDyFVsDhgVpYGxd32GNEoP64nRI;
+        Host=snuffleupagus.db.elephantsql.com;
+        Port=5432;
+        Database=xsxmxiiw;
+        Pooling=true;Min Pool Size=0;Max Pool Size=100;
+        Connection Lifetime=0;
+    ";
 
-        private DatabaseConnector(string connectionString) {
+        private DatabaseConnector(string connectionString)
+        {
             _connectionString = connectionString;
-        }        
-        
-        public static void init(string connectionString)
+        }
+
+        public static void Init(string connectionString)
         {
             new DatabaseConnector(connectionString);
         }
-
-
     }
 }
