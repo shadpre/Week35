6d51c5c4ad598fa81b6c69991dbe993887341f8c
Klavs Tranborg <klavs@tranborg.eu>
2023-08-30 12:19:36 +0200

    a

--- Week35/DatabaseConnector.cs
+++ Week35/DatabaseConnector.cs
@@ -5,15 +5,15 @@ namespace Week35
 {
     public class DatabaseConnector
     {
-        private static string _connectionString = @"
-        User ID=xsxmxiiw;
-        Password=2TYFrLDyFVsDhgVpYGxd32GNEoP64nRI;
-        Host=snuffleupagus.db.elephantsql.com;
-        Port=5432;
-        Database=xsxmxiiw;
-        Pooling=true;Min Pool Size=0;Max Pool Size=100;
-        Connection Lifetime=0;
-    ";
+        private static string _connectionString =
+        "User ID=xsxmxiiw; " +
+        "Password=2TYFrLDyFVsDhgVpYGxd32GNEoP64nRI; " +
+        "Host=snuffleupagus.db.elephantsql.com; " +
+        "Port=5432; " +
+        "Database=xsxmxiiw; " +
+        "Pooling=true;Min Pool Size=0;Max Pool Size=100; " +
+        "Connection Lifetime=0;";
+    
 
         private DatabaseConnector(string connectionString)
         {
